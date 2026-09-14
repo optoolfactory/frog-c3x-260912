9d4d5879929db5b2d79aa4b2e67076f207f45a6b
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-09-14 14:13:49 +0900

    Update helpers.py

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -22,7 +22,7 @@ class DRIVER_MONITOR_SETTINGS:
     self._DT_DMON = DT_DMON
     # ref (page15-16): https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:42018X1947&rid=2
     self._AWARENESS_TIME = 9999. # passive wheeltouch total timeout
-    self._AWARENESS_PRE_TIME_TILL_TERMINAL = 9999.
+    self._AWARENESS_PRE_TIME_TILL_TERMINAL = 9998.
     self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 9999.
     self._DISTRACTED_TIME = 9999. # active monitoring total timeout
     self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 9999.
